e7eeaec257a11b6612485c6ae07663dada427ffd
Łukasz Wójcik <lukasz.wojcik@findwise.com>
2018-01-23 18:01:40 +0100

    Added Create Instance type editor.

---
+++ Findwise.Configuration/TypeEditors/CreateInstanceEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Findwise.Configuration.TypeEditors
+{
+    public class CreateInstanceEditor : UITypeEditor
+    {
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+        {
+            return UITypeEditorEditStyle.Modal;
+        }
+
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+        {
+            if (value != null)
+                return base.EditValue(context, provider, value);
+            else
+                return Activator.CreateInstance(context.PropertyDescriptor.PropertyType);
+        }
+    }
+}
